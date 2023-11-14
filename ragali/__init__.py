4e11d472906ab83de763755f9fb766c1d03f0b7a
Christian Chwala <christian.chwala@kit.edu>
2023-11-14 18:14:14 +0100

    add __init__.py

---
+++ ragali/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for ragali."""
+
+__author__ = """Christian Chwala"""
+__email__ = 'christian.chwala@kit.edu'
+__version__ = '0.0.1'
+
+from . import format_check
+from . import get_datasets
+from . import grid_intersection
+from . import plot_map
+from . import plot_ts
